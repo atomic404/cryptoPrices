a533b32aa5aeb99053fe19d84f7fcbd285b2c266
atomic404 <56467468+atomic404@users.noreply.github.com>
2021-01-08 12:29:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,152 +1,100 @@
-const Discord = require('discord.js');
-const moment = require('moment');
-const DDP = require('ddp');
-const Login = require('ddp-login');
+const axios = require('axios'); require('colors');
+const { change } = require('./title'); change('[CryptoPrices] by Atomic#1337');
+const { yourCryptos, yourPrice, startPrice, flickerTimeMs } = require('./configuration.json');
 
-const client = new Discord.Client();
-const DDPClient = new DDP({url: 'wss://atshop.io/websocket'});
+let allowedCryptos = ["BTC", "BCH", "ETH", "LTC", "ALGO"];
 
-// Insert your discord token here
-const tkn = '';
+let updatedPrice = {};
 
-// https://docs.atshop.io/guide/authentication.html#fetching-your-login-token
-const atshopTkn = '';
-const atshopLink = 'https://yourshop.atshop.io/'; // Make sure you have a '/' at the end
+function getPrice(sign) {
+    axios.post('https://countervalues.live.ledger.com/latest', [{ from: sign, to: "USD" }])
+        .then(res => {
+            updatedPrice[sign] = res.data[0];
+        });
+}
 
-// Prefix for the discord bot.
-const prefix = '$';
-
-// Picture for some random commands
-const pic = 'https://link.com/picture.png';
-
-
-// Connecting to discord.js && atshop.io ws
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-DDPClient.connect((err, reconnected) => {
-    if (err) {
-        throw err;
+function getResponse(func, options = {}) {
+    if (func === "myBalance") {
+        let A = updatedPrice[options.walletType], B = options.wallet[options.walletType], C = startPrice[options.walletType];
+        let bal = (B * A);
+        let percentage = (((A - C) * 100) / A).toFixed(2);
+        percentage = percentage.toString();
+        let percent = percentage.startsWith("-") ? `(${percentage})%`.brightRed : `(${percentage})%`.brightGreen
+        return {
+            bal: bal.toFixed(2),
+            percent: percent,
+            type: options.walletType
+        };
     }
+}
 
-    Login.loginWithToken(DDPClient, atshopTkn, (err, user) => {
-        console.log(`You are now logged in.`)
+function app(myCryptos = []) {
+    myCryptos.forEach((crypto) => {
+        if (!allowedCryptos.includes(crypto)) {
+            console.log(`You're trying to use a not approved crypto. (${crypto})`)
+            return process.exit(1);
+        }
+        getPrice(crypto)
     })
-});
 
-function getOrder(message, args) {
-    const ebed = new Discord.RichEmbed()
-        .setAuthor(message.author.tag, message.author.avatarURL)
-        .setDescription(`Payment is pending - items will be issued to the customer once funds have come through.\nIf you think this is a mistake, and the customer has proven the funds have gone thru, please issue the following command:\n\n\`\`\`${prefix}fulfill order_id\`\`\``)
-        .setTimestamp()
-        .setThumbnail(message.author.avatarURL)
-        .setColor(0xff0000);
-    const ebed2 = new Discord.RichEmbed()
-        .setAuthor(message.author.tag, message.author.avatarURL)
-        .setDescription(`Please use the command in the following format:\n\n\`\`\`${prefix}get order_id\`\`\``)
-        .setTimestamp()
-        .setThumbnail(message.author.avatarURL)
-        .setColor(0xff0000)
+    setInterval(() => {
+        myCryptos.forEach((crypto) => {
+            getPrice(crypto)
+        })
 
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    let orderID = args[0];
-    if (!orderID) return message.channel.send(ebed2);
+        console.clear();
 
-    DDPClient.subscribe('order', [orderID], () => {
-        const collection = DDPClient.collections['shop.orders']; // Array of orders you're subscribed to.
-        const order = collection[orderID]; // Your order.
-
-        let ebc = '';
-        let meaning = {
-            reversed: 'The customer triggered a chargeback for this order, reverting funds back to the customer.',
-            completed: 'Order was paid for and fulfilled.',
-            discarded: 'Order was discarded by the shop administrator.',
-            hold: 'Pending manual review by the shop administrator.'
-        }
-        if (order.status == 'reversed') ebc += meaning.reversed;
-        if (order.status == 'completed') ebc += meaning.completed;
-        if (order.status == 'discarded') ebc += meaning.discarded;
-        if (order.status == 'hold') ebc += meaning.hold;
-
-        if (!order.paid) return message.channel.send(ebed);
-        let ago = moment(order.paidAt).fromNow();
-
-        DDPClient.subscribe('ordered.product', [orderID], () => {
-            const collection = DDPClient.collections['shop.products'];
-            var product = collection[order.productId];
-
-            DDPClient.subscribe('order.items', [orderID], () => {
-                const soldStock = DDPClient.collections['shop.product.stock'];
-                let receivedGoods = '';
-
-                if (order.quantity.length > 5) {
-                    receivedGoods += 'Too many accounts to be displayed here.';
-                } else {
-                    Object.values(soldStock).forEach(acc => {
-                        receivedGoods += `${acc.entry}\n`
-                    });
-                }
-
-                const embed = new Discord.RichEmbed()
-                    .setTitle(`Order ID: ${orderID}`)
-                    .setAuthor(order.email, pic)
-                    .setDescription(`[Click here to go to the order page](${atshopLink}admin/order/${orderID}/items)`)
-                    .setFooter(`${atshopLink} | Purchased ${ago}`)
-                    .addField(`Product Name`, product.name, true)
-                    .addField(`Current Stock`, product.stockCount, true)
-                    .addField(`Payment Gateway`, order.paymentMethod, true)
-                    .addField(`Fulfilled - ${order.fulfilled}`, `${ebc}`, true)
-                    .addField(`Quantity`, `${order.quantity}`, true)
-                    .addField(`Price`, `$${order.paidAmount / 100}`, true)
-                    //.addField(`Received Digital Goods (${order.quantity})`, `\`\`\`${receivedGoods}\`\`\``) // Atshop fucks this up sometimes
-                    .setThumbnail(pic)
-                    .setColor(0xff0000)
-                message.channel.send(embed)
+        if (myCryptos.includes("BTC")) {
+            let json = getResponse("myBalance", {
+                wallet: {
+                    BTC: yourPrice.BTC
+                },
+                walletType: "BTC"
             })
-        });
-    });
-};
-
-function replaceOrder(message, args) {
-    const ebed = new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setDescription(`Payment is pending - items will be issued to the customer once funds have come through.\nIf you think this is a mistake, and the customer has proven the funds have gone thru, please issue the following command:\n\n\`\`\`${prefix}fulfill order_id\`\`\``).setTimestamp().setThumbnail(message.author.avatarURL).setColor(0xff0000);
-    const ebed2 = new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setDescription(`Please use the command in the following format:\n\n\`\`\`${prefix}get order_id\`\`\``).setTimestamp().setThumbnail(message.author.avatarURL).setColor(0xff0000)
-    const ebed3 = new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setDescription(`No stock found for this product.`).setTimestamp().setThumbnail(message.author.avatarURL).setColor(0xff0000)
-    const ebed4 = new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL).setDescription(`Replacement has been sent to email.`).setTimestamp().setThumbnail(message.author.avatarURL).setColor(0xff0000)
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    let orderID = args[0];
-    let amount = parseInt(args[1]) || 1;
-    if (!orderID) return message.channel.send(ebed2);
-
-    DDPClient.subscribe('order', [orderID], () => {
-        const collection = DDPClient.collections['shop.orders']; // Array of orders you're subscribed to.
-        const order = collection[orderID]; // Your order.
-
-        if (!order.paid) return message.channel.send(ebed);
-        DDPClient.call('admin.orders.replace', [order.shopId, orderID, amount, 'replaced thru discord bot.'], (err) => {
-            if (err) {
-                console.error(err)
-                return message.channel.send(ebed3)
-            };
-            message.channel.send(ebed4)
-        });
-    });
-};
-
-client.on("message", async(message) => {
-    if (message.author.bot) return;
-    if (!message.guild) return;
-
-    if (message.content.startsWith(prefix)) {
-        let args = message.content.substring(prefix.length).split(" ");
-        let command = args.shift();
-        switch (command) {
-            case "get":
-                return getOrder(message, args);
-            case "replace":
-                return replaceOrder(message, args);
+            console.log(`[${json.type}] `.brightBlue + `Market Price: `.brightWhite + `$${Math.round(updatedPrice[json.type])} `.cyan + json.percent)
+            console.log(`[${json.type}] `.brightBlue + `Your wallet: `.brightWhite + `$${json.bal} `.yellow + '\n')
         }
-    }
-});
+        if (myCryptos.includes("BCH")) {
+            let json = getResponse("myBalance", {
+                wallet: {
+                    BCH: yourPrice.BCH
+                },
+                walletType: "BCH"
+            })
+            console.log(`[${json.type}] `.brightBlue + `Market Price: `.brightWhite + `$${updatedPrice[json.type]} `.cyan + json.percent)
+            console.log(`[${json.type}] `.brightBlue + `Your wallet: `.brightWhite + `$${json.bal} `.yellow + '\n')
+        }
+        if (myCryptos.includes("ETH")) {
+            let json = getResponse("myBalance", {
+                wallet: {
+                    ETH: yourPrice.ETH
+                },
+                walletType: "ETH"
+            })
+            console.log(`[${json.type}] `.brightBlue + `Market Price: `.brightWhite + `$${Math.round(updatedPrice[json.type])} `.cyan + json.percent)
+            console.log(`[${json.type}] `.brightBlue + `Your wallet: `.brightWhite + `$${json.bal} `.yellow + '\n')
+        }
+        if (myCryptos.includes("LTC")) {
+            let json = getResponse("myBalance", {
+                wallet: {
+                    LTC: yourPrice.LTC
+                },
+                walletType: "LTC"
+            })
+            console.log(`[${json.type}] `.brightBlue + `Market Price: `.brightWhite + `$${updatedPrice[json.type]} `.cyan + json.percent)
+            console.log(`[${json.type}] `.brightBlue + `Your wallet: `.brightWhite + `$${json.bal} `.yellow + '\n')
+        }
+        if (myCryptos.includes("ALGO")) {
+            let json = getResponse("myBalance", {
+                wallet: {
+                    ALGO: yourPrice.ALGO
+                },
+                walletType: "ALGO"
+            })
+            console.log(`[${json.type}] `.brightBlue + `Market Price: `.brightWhite + `$${updatedPrice[json.type]} `.cyan + json.percent)
+            console.log(`[${json.type}] `.brightBlue + `Your wallet: `.brightWhite + `$${json.bal} `.yellow + '\n')
+        }
+    }, flickerTimeMs);
+}
 
-client.login(tkn);
+app(yourCryptos)
